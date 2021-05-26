89ee7bfc3559de7a9faa0f66dfa0c1ab11f56abb
연훈 배 <sbaru1997@naver.com>
2021-05-27 03:42:38 +0900

    21.05.27 -1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,11 +33,14 @@ let videos = [
     const video = videos[id - 1];
     return res.render("watch", { pageTitle: `Watching: ${video.title}` , video});
   };
-  export const edit = (req, res) => {
+  export const getEdit = (req, res) => {
     const { id } = req.params;
     const video = videos[id - 1];
     return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
   }
-  export const search = (req, res) => res.send("Search");
-  export const upload = (req, res) => res.send("Upload");
-  export const deleteVideo = (req, res) => res.send("Delete Video");
+  export const postEdit = (req, res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+  };

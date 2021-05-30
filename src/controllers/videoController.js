8279efb7879e16ee04d9fa82c23156d7ebc1f8fd
연훈 배 <sbaru1997@naver.com>
2021-05-30 20:59:40 +0900

    Add database middleware

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -19,11 +19,24 @@
   export const getEdit = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    if(!video){
+      return res.render("404", {pageTitle: "Video not found."})
+    }
     return res.render("edit", {pageTitle: `Edit: ${video.title}`, video});
   }
-  export const postEdit = (req, res) => {
+  export const postEdit = async (req, res) => {
     const {id} = req.params;
-    const {title} = req.body;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.exists({_id: id});
+    if(!video){
+      return res.render("404", {pageTitle: "Video not found."})
+    }
+    await Video.findByIdAndUpdate(id, {
+      title, description,
+      hashtags:hashtags
+      .split(",")
+      .map((word) => (word.startsWith(`#`) ?  word : `#${word}`)),
+    });
     return res.redirect(`/videos/${id}`);
   };
 
@@ -36,11 +49,7 @@
       const video = new Video({
       title : title,
       description : description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
-      meta:{
-        views: 0,
-        rating: 0,
-      },
+      hashtags,
     });
     await video.save();
     return res.redirect("/");
